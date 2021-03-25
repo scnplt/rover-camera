e0bb7f5d59a81c1920ba2e9975351547548fbf8b
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2021-03-25 22:53:38 +0300

    Create model classes for api response

---
+++ app/src/main/java/dev/sertan/android/rovercamera/data/model/Camera.kt
@@ -0,0 +1,12 @@
+package dev.sertan.android.rovercamera.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Camera(
+    val id: Int,
+    val name: String,
+    @SerializedName("rover_id")
+    val roverId: Int,
+    @SerializedName("full_name")
+    val fullName: String
+)
---
+++ app/src/main/java/dev/sertan/android/rovercamera/data/model/Node.kt
@@ -0,0 +1,5 @@
+package dev.sertan.android.rovercamera.data.model
+
+data class Node(
+    val photos: List<Photo>
+)
