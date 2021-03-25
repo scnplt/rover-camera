3bcda7871fc3c59fdfccf729a1b258f8f4fb082e
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-03-25 23:08:10 +0300

    Create api class

---
+++ app/src/main/java/dev/sertan/android/rovercamera/data/api/NasaApi.kt
@@ -0,0 +1,18 @@
+package dev.sertan.android.rovercamera.data.api
+
+import dev.sertan.android.rovercamera.data.model.Node
+import dev.sertan.android.rovercamera.util.NasaApiUtils
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NasaApi {
+    @GET("photos")
+    fun getNode(
+        @Query("sol") sol: Int? = null,
+        @Query("earth_date") earthDate: String? = null,
+        @Query("page") page: Int? = null,
+        @Query("camera") camera: String? = null,
+        @Query("api_key") apiKey: String = NasaApiUtils.API_KEY
+    ): Call<Node>
+}
