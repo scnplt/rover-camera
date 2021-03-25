a91ce3f5e53f38648486632b6c55bd63ed47435a
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2021-03-25 23:59:37 +0300

    Create repository and change the response type to nullable Node in NasaApi

--- app/src/main/java/dev/sertan/android/rovercamera/util/State.kt
+++ app/src/main/java/dev/sertan/android/rovercamera/util/State.kt
@@ -3,6 +3,6 @@ package dev.sertan.android.rovercamera.util
 sealed class State {
     object IDLE : State()
     object LOADING : State()
-    data class ERROR(val msg: String) : State()
+    object ERROR : State()
     data class LOADED<T>(val data: T?) : State()
 }
