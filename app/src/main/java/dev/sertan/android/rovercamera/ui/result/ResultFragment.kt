3253357161798e9abd06b449293b68d45e36c144
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-03-26 13:15:04 +0300

    Create MainActivity fragments

---
+++ app/src/main/java/dev/sertan/android/rovercamera/ui/result/ResultFragment.kt
@@ -0,0 +1,9 @@
+package dev.sertan.android.rovercamera.ui.result
+
+import dev.sertan.android.rovercamera.R
+import dev.sertan.android.rovercamera.databinding.FragmentResultBinding
+import dev.sertan.android.rovercamera.ui.base.BaseFragment
+
+class ResultFragment : BaseFragment<FragmentResultBinding>() {
+    override fun getLayoutId(): Int = R.layout.fragment_result
+}
---
+++ app/src/main/java/dev/sertan/android/rovercamera/ui/search/SearchFragment.kt
@@ -0,0 +1,9 @@
+package dev.sertan.android.rovercamera.ui.search
+
+import dev.sertan.android.rovercamera.R
+import dev.sertan.android.rovercamera.databinding.FragmentSearchBinding
+import dev.sertan.android.rovercamera.ui.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>() {
+    override fun getLayoutId(): Int = R.layout.fragment_search
+}
