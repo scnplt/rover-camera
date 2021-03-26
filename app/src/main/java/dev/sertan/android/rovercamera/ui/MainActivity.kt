d59106ed0ad3711e60897d2ee5d95a49236d9db2
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-03-26 11:19:25 +0300

    Create MainActivity and add it to AndroidManifest

---
+++ app/src/main/java/dev/sertan/android/rovercamera/ui/MainActivity.kt
@@ -0,0 +1,9 @@
+package dev.sertan.android.rovercamera.ui
+
+import dev.sertan.android.rovercamera.R
+import dev.sertan.android.rovercamera.databinding.ActivityMainBinding
+import dev.sertan.android.rovercamera.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun getLayoutId(): Int = R.layout.activity_main
+}
