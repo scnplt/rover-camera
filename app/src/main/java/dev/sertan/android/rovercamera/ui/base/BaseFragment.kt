83c44a5b8f2fb9d71341b37d412a4b809b90d545
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-03-26 11:01:22 +0300

    Create base activity and base fragment classes

---
+++ app/src/main/java/dev/sertan/android/rovercamera/ui/base/BaseFragment.kt
@@ -0,0 +1,26 @@
+package dev.sertan.android.rovercamera.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<DB : ViewDataBinding> : Fragment() {
+    lateinit var binding: DB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false)
+        return binding.root
+    }
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+}
